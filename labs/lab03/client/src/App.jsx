1ccf499c374139f227c239cfa9a0b07fd1cc24ba
lisakikuch <lisathichanpho@gmail.com>
2025-03-08 08:42:34 -0500

    Submit Lab3

---
+++ labs/lab03/client/src/App.jsx
@@ -0,0 +1,188 @@
+import { useState } from "react";
+
+const App = () => {
+  // what do we need to track
+  // setters for saving
+  const [singleFile, setSingleFile] = useState(null);
+  const [multipleFiles, setMultipleFiles] = useState([]);
+
+  // getters for displaying
+  const [displayImage, setDisplayImage] = useState(null); // Display single image
+  const [displayImages, setDisplayImages] = useState([]); // Display multiple image
+  const [displayDogImage, setDisplayDogImage] = useState(null);
+
+  const [message, setMessage] = useState("");
+
+  // Handlers
+  const handleSingleFileChange = (e) => {
+    if (e.target.files.length > 0) {
+      setSingleFile(e.target.files[0]);
+      setMessage("Please select a file before uploading");
+      return;
+    }
+  };
+
+  // fetch functions -> fetch a random single image
+  const fetchSingleFile = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/fetch/single`);
+      // The information we get is images in the format called blob not JSON
+
+      const blob = await response.blob(); // we made a blob - Binary Large Object
+      // but thats not an image, so we need to make an image element
+
+      // using createObjectURL
+      const imageUrl = URL.createObjectURL(blob);
+      setDisplayImage(imageUrl);
+    } catch (error) {
+      console.error("Error fetching single file:", error);
+    }
+  };
+
+  // fetch functions -> save single
+  const handleSubmitSingleFile = async (e) => {
+    e.preventDefault();
+
+    if (!singleFile) {
+      setMessage("Please select a file before uploading.");
+      return;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append("file", singleFile);
+
+      const response = await fetch(`http://localhost:8000/save/single`, {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error || "Image upload failed");
+      }
+      setMessage("File uploaded successfully!");
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  // fetch functions -> save multiple [TODO]
+  // fetch functions -> fetch multiple [TODO]
+  const fetchMultiple = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:8000/fetch/multiple`);
+      const data = await response.json();
+      console.log(data);
+
+      setMultipleFiles(data);
+
+      const filePromises = data.map(async (filename) => {
+        const fetchFilenameData =
+          await fetch(`http://localhost:8000/fetch/file/${filename}`);
+        const fileBlob = await fetchFilenameData.blob();
+        return URL.createObjectURL(fileBlob);
+      });
+
+      const imageUrls = await Promise.all(filePromises);
+      setDisplayImages(imageUrls);
+
+    } catch (error) {
+      console.log(error);
+    };
+
+  }
+
+  // fetch functions -> fetch dog image [TODO]
+  const fetchDogImage = async () => {
+
+    try {
+      const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
+      const data = await response.json();
+
+      setDisplayDogImage(data.message);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // fetch functions -> save dog image [TODO]
+  const saveDogImage = async (e) => {
+    e.preventDefault();
+
+    // Error handling - check if the image is there
+    try {
+
+      // fetch the dog image, get the blob
+      const response = await fetch(displayDogImage);
+      const blob = await response.blob();
+
+      // save the blob in form data
+      const formData = new FormData();
+      formData.append("file", blob, "dog-image.jpg");
+
+      // upload formData
+      const uploadResponse = await fetch(`http://localhost:8000/save/single`, {
+        method: "POST",
+        body: formData
+      });
+
+      const data = uploadResponse.json();
+      setMessage(data.message);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <p>{message}</p>
+      <h2>Fetch Single Random Image</h2>
+      <button onClick={fetchSingleFile}>Fetch Single File</button>
+      {displayImage && (
+        <div>
+          <h3>Single File</h3>
+          <img
+            src={displayImage}
+            alt="Display Image"
+            style={{ width: "200px", marginTop: "10px" }}
+          />
+        </div>
+      )}
+      <form onSubmit={handleSubmitSingleFile}>
+        <h2>Upload Single File</h2>
+        <input type="file" onChange={handleSingleFileChange} />
+        <button type="submit">Upload Single File</button>
+      </form>
+      <button onClick={fetchMultiple}>Grab multiple files</button>
+      {/* Conditional rendering
+      {condition ? (true) : (false)} */}
+      {displayImages.length > 0 ? (
+        displayImages.map((imageUrl, index) => (
+          <div key={index}>
+            <img
+              src={imageUrl}
+            />
+          </div>
+        ))
+      ) : (
+        <p>No image to display</p>
+      )}
+      <button onClick={fetchDogImage}>Fetch a dog image</button>
+      {displayDogImage && (
+        <div>
+          <img
+            src={displayDogImage}
+            style={{ width: "200px", margin: "10px" }}
+          />
+          <button onClick={saveDogImage}>Save for later</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ labs/lab03/server/middleware/multer.js
@@ -0,0 +1,23 @@
+import multer from "multer";
+// Handles multipart/form-data, which is primarily used for uploading files in Express applications
+import path from "path";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  // the function that saves the file
+  destination: function (req, file, cb) {
+    // where we are storing the file
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: function (req, file, cb) {
+    // what is the filename going to be
+    cb(null, `${Date.now()}-${file.originalname}`); // this will save files like: date-filename.extension
+  },
+});
+
+const upload = multer({ storage: storage }); // the middleware function that handles uploading
+
+export default upload;
